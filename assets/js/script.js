a216651a615f8275906bff0a64f29abbc532c3e2
Dajan Plackovic <plackovic.dajan@gmail.com>
2024-02-24 14:06:55 +0000

    feat: add meteor movement function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,11 @@
-createMeteor()
+document.addEventListener('DOMContentLoaded', function () {
+  createMeteor()
+  setInterval(tick, 50)
+})
+
+function tick() {
+  moveMeteors()
+}
 
 /**
  * Creates a meteor with a random X position at the top of the game area
@@ -9,4 +16,21 @@ function createMeteor() {
   newMeteor.style.top = '0px'
   newMeteor.style.left = `${Math.random() * 90}%`
   document.getElementById('game-area').appendChild(newMeteor)
+}
+
+function moveMeteors() {
+  const disappearingHeight =
+    +window
+      .getComputedStyle(document.getElementById('game-area'))
+      .height.slice(0, -2) * 1.1 // 10% added onto height as meteor disappears a little too early otherwise
+  const meteors = document.getElementsByClassName('meteor')
+
+  for (meteor of meteors) {
+    const newTop = +meteor.style.top.slice(0, -2) + 10
+    if (newTop > disappearingHeight) {
+      meteor.remove()
+    } else {
+      meteor.style.top = `${newTop}px`
+    }
+  }
 }
